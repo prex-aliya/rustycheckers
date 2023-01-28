57b59a7f1a4e27deb72522123092d54e747dd975
prex-aliya <droved-through@proton.me>
2023-01-28 02:27:24 -0600

    🐲got simple board printed

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use std::process;
 
 use ncurses::*;
 
-const BLACK_BOARD_PAIR: i16 = 1;
-const WHITE_BOARD_PAIR: i16 = 1;
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
 
 fn error(msg: &str) {
     endwin();
@@ -33,17 +33,21 @@ impl Turn {
 /* ui {{{ */
 #[derive(Default)]
 struct Ui {
+    row: i32,
+    col: i32
 }
 
 impl Ui {
-    fn begin(&mut self, x:usize, y:usize) {
+    fn begin(&mut self, x:i32, y:i32) {
+        self.row = y;
+        self.col = x;
 
+        mv(y, x);
     }
     fn notifications(&mut self) {
-
     }
     fn print_board(&mut self) {
-
+        self.row = 3;
     }
     fn status(&mut self, turn: Turn) {
 
@@ -63,30 +67,64 @@ impl Ui {
 /* }}}*/
 
 fn main() {
-    if has_colors() == false {
-        error("Terminal Doess not support color");
-    }
-
     initscr();
     noecho();
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE)           ;
 
-    start_color();
-    /*          pair            forground   background */
-    init_pair(BLACK_BOARD_PAIR, COLOR_WHITE, COLOR_RED);
-    init_pair(WHITE_BOARD_PAIR, COLOR_BLACK, COLOR_WHITE);
 
-    let mut turn: Turn = Turn::White;
+    start_color();
+    if has_colors() == false {
+        endwin();
+        error("Terminal Doess not support color");
+    }
+
+    /*          pair            forground   background */
+    init_pair(1, COLOR_WHITE, COLOR_RED);
+    init_pair(2, COLOR_BLACK, COLOR_WHITE);
+
+
+
+    let turn: Turn = Turn::White;
     let mut ui = Ui::default();
+
+    ui.print_board();
 
     let mut quit = false;
     while !quit {
+
+        let mut row: i32 = 1;
+
+        for _ in 0..4 {
+            for _ in 0..3 {
+                mv(row, 0);
+                for _ in 0..4 {
+                    attron(COLOR_PAIR(1));
+                    addstr("     ");
+
+                    attron(COLOR_PAIR(2));
+                    addstr("     ");
+                }
+                row += 1;
+            }
+            for _ in 0..3 {
+                mv(row, 0);
+                for _ in 0..4 {
+                    attron(COLOR_PAIR(2));
+                    addstr("     ");
+
+                    attron(COLOR_PAIR(1));
+                    addstr("     ");
+                }
+                row += 1;
+            }
+        }
+
+
         ui.begin(0,0);
         {
             ui.notifications();
 
             /* chess board */
-            ui.print_board();
 
             /* statistics */
             match turn {
